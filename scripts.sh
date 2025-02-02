1ba5cb5ec23470be736858c2f00ef914de552dbf
Daniel Calderon <danycalvir@gmail.com>
2025-02-01 23:00:57 -0700

    persist data in mongo

---
+++ scripts.sh
@@ -0,0 +1,5 @@
+# 05 network
+docker build -t goals-node .
+docker run --name goals-backend --rm -d -p 80:80 --network goals-net goals-node
+docker run --name goals-frontend --rm -p 3000:3000 -it goals-react         
+docker run --name mongodb -v data:/data/db --rm -d -e MONGO_INITDB_ROOT_USERNAME=dacv -e MONGO_INITDB_ROOT_PASSWORD=secret --network goals-net mongo
