5743c45854d9b830a29cf3c1128bbd42a2daa8d3
Daniel Calderon <danycalvir@gmail.com>
2025-02-02 22:59:56 -0700

    persit data in backend

--- scripts.sh
+++ scripts.sh
@@ -1,5 +1,9 @@
 # 05 network
 docker build -t goals-node .
-docker run --name goals-backend --rm -d -p 80:80 --network goals-net goals-node
+
+ docker run --name goals-backend -v C:\Users\danyc\Documents\learning-docker\05-multi-01\backend:/app -v logs:/app/logs --rm -d -p 80:80 --network goals-net goals-node
+
+
+
 docker run --name goals-frontend --rm -p 3000:3000 -it goals-react         
 docker run --name mongodb -v data:/data/db --rm -d -e MONGO_INITDB_ROOT_USERNAME=dacv -e MONGO_INITDB_ROOT_PASSWORD=secret --network goals-net mongo
