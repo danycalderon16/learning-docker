2b7ec63da46cf75c3c2bbd1fd32a60dad6fd170e
Daniel Calderon <danycalvir@gmail.com>
2025-02-03 23:04:07 -0700

    init docker compose

--- scripts.sh
+++ scripts.sh
@@ -2,5 +2,5 @@
 docker build -t goals-node .
 
 docker run --name goals-backend -v C:\Users\danyc\Documents\learning-docker\05-multi-01\backend:/app -v logs:/app/logs -v /app/node_modules --rm -d -p 80:80 -e MONGODB_USERNAME=dacv --network goals-net goals-node
-docker run --name goals-frontend --rm -p 3000:3000 -it goals-react         
+docker run -v C:\Users\danyc\Documents\learning-docker\05-multi-01\frontend\src:/app/src  --name goals-frontend --rm -p 3000:3000 -it goals-react       
 docker run --name mongodb -v data:/data/db --rm -d -e MONGO_INITDB_ROOT_USERNAME=dacv -e MONGO_INITDB_ROOT_PASSWORD=secret --network goals-net mongo
